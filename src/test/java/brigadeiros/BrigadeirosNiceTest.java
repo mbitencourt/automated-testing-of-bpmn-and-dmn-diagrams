ca850000c7268a8a257cab9f04fa6f572ce6db35
carlosrenatoramos <carlosrenatoramos@gmail.com>
2021-05-30 00:02:55 -0300

    Atualização para Cucumber Reports

--- src/test/java/brigadeiros/BrigadeirosNiceTest.java
+++ src/test/java/brigadeiros/BrigadeirosNiceTest.java
@@ -1,12 +1,40 @@
-package postalleg;
+package brigadeiros;
 
-import org.junit.runner.RunWith;
-import com.intuit.karate.junit4.Karate;
-import cucumber.api.CucumberOptions;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@RunWith(Karate.class)
-@CucumberOptions(features="src/test/java")
-public class BrigadeirosNiceTest {
-    // refer to https://github.com/intuit/karate#naming-conventions
-    // for folder-structure recommendations and naming conventions
-} 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.Test;
+
+import com.intuit.karate.Results;
+import com.intuit.karate.Runner;
+
+import net.masterthought.cucumber.Configuration;
+import net.masterthought.cucumber.ReportBuilder;
+
+class BrigadeirosNiceTest {
+
+	@Test
+	void testParallel() {
+		System.setProperty("karate.env", "demo");
+		Results results = Runner.path("classpath:brigadeiros").tags("~@ignore")
+				.outputCucumberJson(true)
+				.parallel(5);
+		generateReport(results.getReportDir());
+		assertEquals(0, results.getFailCount(), results.getErrorMessages());
+	}
+
+	public static void generateReport(String karateOutputPath) {
+		Collection<File> jsonFiles = FileUtils.listFiles(new File(karateOutputPath), new String[] { "json" }, true);
+		List<String> jsonPaths = new ArrayList<String>(jsonFiles.size());
+		jsonFiles.forEach(file -> jsonPaths.add(file.getAbsolutePath()));
+		Configuration config = new Configuration(new File("target"), "demo");
+		ReportBuilder reportBuilder = new ReportBuilder(jsonPaths, config);
+		reportBuilder.generateReports();
+	}
+
+}
